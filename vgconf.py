2914800ef0361aac89724b1d3647d47283a9dfe7
Prasanna Patil <prasannapatil08@gmail.com>
2018-02-06 19:49:43 +0530

    Initial setup

---
+++ vgconf.py
@@ -0,0 +1,6 @@
+"""Configuration parameters and constants for Vigilancia."""
+
+import os
+
+# Base path where trained classifiers stored.
+CLASSIFIER_DATA_PATH = os.path.join('data', 'classifiers')
