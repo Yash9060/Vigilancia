fb3659a0918342525c8083f85726b6a638e5c911
Prasanna Patil <prasannapatil08@gmail.com>
2018-03-06 16:46:34 +0530

    Organize main module.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+"""Main module of Vigilancia. This where execution begins."""
 import vg_config
 vg_config.init()
 
 from core.domain import DisplayScreen
 import sys
 
-dsp = DisplayScreen.DisplayScreen(sys.argv)
-dsp.create_application()
-sys.exit(dsp.start_app())
+if __name__ == '__main__':
+    dsp = DisplayScreen.DisplayScreen(args)
+    dsp.create_application()
+    sys.exit(dsp.start_app())
