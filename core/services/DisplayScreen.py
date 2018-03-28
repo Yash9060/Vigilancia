eb37994b39286467c0ceed710d932bd84eebb758
Prasanna Patil <prasannapatil08@gmail.com>
2018-03-28 08:10:05 +0530

    Add audio alerts

--- core/services/DisplayScreen.py
+++ core/services/DisplayScreen.py
@@ -162,6 +162,11 @@ class DisplayScreen(object):
         self.event_detector_prediction = []
         self.activity_detector_prediction = None
 
+        # Audio alert file.
+        self.alert_beep = self.qt.get_phonon_media_source(
+            vgconf.ALERT_BEEP_FILE)
+        self.is_beep_playing = False
+
     def add_graphics_view(
             self, parent, name, geometry, stylesheet, interactive):
         obj = self.qt.get_graphics_widget(parent)
@@ -400,6 +405,11 @@ class DisplayScreen(object):
             self.activity_detected_view_geometry,
             self.detection_view_stylesheet)
 
+        # Audio player.
+        self.alert_beep_player = self.qt.get_phonon_audio_player(
+            self.central_widget)
+        self.alert_beep_player.setCurrentSource(self.alert_beep)
+
     def raise_ui_components(self):
         # Raise all components on main UI.
         self.alert_window.raise_()
@@ -480,6 +490,10 @@ class DisplayScreen(object):
             self.activity_detected_view, 'MainWindow',
             self.activity_detected_view_text, None)
 
+    def _start_alert(self):
+        self._start_visual_alert()
+        self._start_audio_alert()
+
     def _flash_alert(self):
         self.alert_count -= 1
         if self.alert_count == 0:
@@ -495,6 +509,16 @@ class DisplayScreen(object):
             self.alert_timer = self.get_timer(
                 self._flash_alert, self._flash_alert_update_rate)
         self.alert_count = vgconf.DEFAULT_ALERT_FLASH_COUNT
+
+    def _audio_alert_finished(self):
+        self.is_beep_playing = False
+
+    def _start_audio_alert(self):
+        if self.is_beep_playing:
+            return
+        self.is_beep_playing = True
+        self.alert_beep_player.stop()
+        self.alert_beep_player.play()
 
     def _object_detection_slider_value_changed(self):
         value = self.object_detection_slider.value()
@@ -542,7 +566,7 @@ class DisplayScreen(object):
 
         # Start alert if suspicious object is detected.
         if detected_suspicious_objects:
-            self._start_visual_alert()
+            self._start_alert()
 
     def _update_detceted_events(self, events_prediction):
         events = ', '.join(events_prediction)
@@ -557,7 +581,7 @@ class DisplayScreen(object):
                 detected_suspicious_events = True
 
         if detected_suspicious_events:
-            self._start_visual_alert()
+            self._start_alert()
 
     def _update_detected_activity(self, activity_prediction):
         self.activity_detected_view_text = activity_prediction
@@ -566,7 +590,7 @@ class DisplayScreen(object):
             self.activity_detected_view_text, None)
 
         if activity_prediction == vgconf.ABNORMAL_ACTIVITY:
-            self._start_visual_alert()
+            self._start_alert()
 
     def _update_stream_name_label(self):
         filename = 'webcam'
@@ -691,6 +715,8 @@ class DisplayScreen(object):
             self.file_selection_button, 'clicked()', self._open_file_dialog)
         self.qt.connect_obj_event(
             self.clear_button, 'clicked()', self._clear_stream)
+        self.qt.connect_obj_event(
+            self.alert_beep_player, 'finished()', self._audio_alert_finished)
 
     def create_application(self):
         self.create_ui_components()
