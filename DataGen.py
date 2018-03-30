3d463588bd0942929cd4ee32fe5ea6d7e462ded2
Yash9060 <yashshah9060@gmail.com>
2018-03-30 12:15:58 +0530

    Minor Change

--- DataGen.py
+++ DataGen.py
@@ -17,11 +17,4 @@ for i in range(len(Username)):
 		'Password':Password[i]
 	}
 	result=db.Logindata.insert_one(User)
-	#print("Created one entry with ID {0} ".format(result.inserted_id))
-print("Db created and data populated")
-# dumping bson and json file of all collections inside User DB
-
-path = os.getcwd()
-os.system('mongodump -d Users -o path')
-print("Dump Done")
 
